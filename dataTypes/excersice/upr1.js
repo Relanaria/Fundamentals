cac7d62e0a76b3867a3ae1de16aadd8c772b2795
Relanaria <shadowword9@gmail.com>
2023-09-23 13:33:51 +0300

    update

--- dataTypes/excersice/upr1.js
+++ dataTypes/excersice/upr1.js
@@ -1,3 +1,12 @@
-function sumDigits(num){
-    
+function sumDigits(num) {
+  let Str = num.toString();
+  let sum = 0;
+
+  for (let i = 0; i < Str.length; i++) {
+    sum += Number(Str[i]);
+  }
+
+  console.log(sum);
 }
+
+sumDigits(543);
