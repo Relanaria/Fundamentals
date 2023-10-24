4334db699500d5c80c287aabc3607104403613ad
Relanaria <shadowword9@gmail.com>
2023-10-24 14:55:16 +0300

    obcjetsAndClasses

--- objectsAndClasses/Exercise/movies.js
+++ objectsAndClasses/Exercise/movies.js
@@ -3,22 +3,58 @@ function movies(arr) {
 
   class kino {
     constructor(name, director, date) {
-      this.name = name;
+      this.name = name.trim();
       this.director = director;
       this.date = date;
     }
   }
 
-  movies.push(new kino("kiro", "kirev", 5))
-
   for (const element of arr) {
-    console.log(element.name);
+    let command = element.split(" ");
+    let index = command.indexOf("directedBy");
+    let index2 = command.indexOf("onDate");
+
+    if (command[0] === "addMovie") {
+      let movieName = command.slice(1).join(" ");
+      movies.push(new kino(movieName, undefined, undefined));
+    }else if (command[index] === "directedBy") {
+      let nameOfMovie = command.splice(0, index).join(" ");
+      index = command.indexOf("directedBy");
+      movies.forEach((element) => {
+        if (element.name === nameOfMovie) {
+          element.director = command.slice(index + 1).join(" ");
+        }
+      });
+    }else if (command[index2] === "onDate") {
+      let nameOfMovie = command.splice(0, index2).join(" ");
+      index = command.indexOf("directedBy");
+      movies.forEach((element) => {
+        if (element.name === nameOfMovie) {
+          element.date = command[command.length - 1];
+        }
+      });
+    }
   }
+
+  movies.forEach((key) => {
+    if (
+      key.name !== undefined &&
+      key.date !== undefined &&
+      key.director !== undefined
+    ) {
+      console.log(JSON.stringify(movies));
+    }
+  });
 }
 
 movies([
-  "Godfather onDate 29.07.2018",
-  "Fast and Furious onDate 30.07.2018",
-  "Batman onDate 01.08.2018",
-  "Fast and Furious directedBy Rob Cohen",
-]);
+  'addMovie Fast and Furious',
+  'addMovie Godfather',
+  'Inception directedBy Christopher Nolan',
+  'Godfather directedBy Francis Ford Coppola',
+  'Godfather onDate 29.07.2018',
+  'Fast and Furious onDate 30.07.2018',
+  'Batman onDate 01.08.2018',
+  'Fast and Furious directedBy Rob Cohen'
+  ])
+  ;
