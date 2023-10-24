b8d4e24c981af700b497d4e1665c43d8a2ea6027
Relanaria <shadowword9@gmail.com>
2023-10-24 14:54:57 +0300

    objects

--- objectsAndClasses/Exercise/movies.js
+++ objectsAndClasses/Exercise/movies.js
@@ -17,10 +17,6 @@ function movies(arr) {
 }
 
 movies([
-  "addMovie Fast and Furious",
-  "addMovie Godfather",
-  "Inception directedBy Christopher Nolan",
-  "Godfather directedBy Francis Ford Coppola",
   "Godfather onDate 29.07.2018",
   "Fast and Furious onDate 30.07.2018",
   "Batman onDate 01.08.2018",
